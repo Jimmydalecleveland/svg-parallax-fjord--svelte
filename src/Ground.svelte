0468d437374eace79f1c390cfb23254ebd071bc0
Jimmy DC <jimmydalecleveland@gmail.com>
2020-06-07 20:23:39 -0600

    change direction to svg components
    using svelte:component we can loop over an array and render each
    svg as a component, allowing us to render the inline svg code rather
    than image tags, and benefit from local scoping of styles.

---
+++ src/Ground.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let style
+</script>
+
+<style>
+  svg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    will-change: transform;
+  }
+  .cls-1 {
+    fill: #a4c443;
+  }
+</style>
+
+<svg
+  {style}
+  id="ground"
+  xmlns="http://www.w3.org/2000/svg"
+  width="2560"
+  height="1440"
+  viewBox="0 0 2560 1440">
+  <title>ground-3</title>
+  <rect class="cls-1" y="1152" width="2560" height="150" />
+</svg>
--- src/ParallaxDemo.svelte
+++ src/ParallaxDemo.svelte
@@ -1,17 +1,22 @@
 <script>
+  import Mountains from './Mountains.svelte'
+  import Hills from './Hills.svelte'
+  import Trees from './Trees.svelte'
+  import Ground from './Ground.svelte'
+
   const layers = [
-    { fileName: 'cloud-2-light.svg', layer: 0 },
-    { fileName: 'cloud-4-light.svg', layer: 0 },
-    { fileName: 'cloud-5-light.svg', layer: 0 },
-    { fileName: 'mountains.svg', layer: 1 },
-    { fileName: 'cloud-1-dark.svg', layer: 2 },
-    { fileName: 'cloud-3-dark.svg', layer: 2 },
-    { fileName: 'hills.svg', layer: 3 },
-    { fileName: 'trees.svg', layer: 4 },
-    { fileName: 'ground.svg', layer: 5 },
-    { fileName: 'wave-3.svg', layer: 6 },
-    { fileName: 'wave-2.svg', layer: 7 },
-    { fileName: 'wave-1.svg', layer: 8 },
+    // { component: 'cloud-2-light.svg', layer: 0 },
+    // { component: 'cloud-4-light.svg', layer: 0 },
+    // { component: 'cloud-5-light.svg', layer: 0 },
+    { component: Mountains, layer: 1 },
+    // { component: 'cloud-1-dark.svg', layer: 2 },
+    // { component: 'cloud-3-dark.svg', layer: 2 },
+    { component: Hills, layer: 3 },
+    { component: Trees, layer: 4 },
+    { component: Ground, layer: 5 },
+    // { component: 'wave-3.svg', layer: 6 },
+    // { component: 'wave-2.svg', layer: 7 },
+    // { component: 'wave-1.svg', layer: 8 },
   ]
 
   let y
@@ -26,38 +31,12 @@
     transform: translate(-50%, 0);
   }
 
-  .parallax-container img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    will-change: transform;
-  }
-
   .parallax-container img:last-child::after {
     content: '';
     position: absolute;
     width: 100%;
     height: 100%;
     background: rgb(45, 10, 13);
-  }
-
-  .text {
-    position: relative;
-    width: 100%;
-    height: 300vh;
-    color: rgb(220, 113, 43);
-    text-align: center;
-    padding: 4em 0.5em 0.5em 0.5em;
-    box-sizing: border-box;
-    pointer-events: none;
-  }
-
-  span {
-    display: block;
-    font-size: 1em;
-    text-transform: uppercase;
-    will-change: transform, opacity;
   }
 
   .foreground {
@@ -81,15 +60,10 @@
 
 <main class="parallax-container">
   {#each layers as layer}
-    <img
-      src="assets/{layer.fileName}"
-      alt={layer.fileName}
+    <svelte:component
+      this={layer.component}
       style="transform: translate(0, {(-y * layer.layer) / (layers.length - 1)}px" />
   {/each}
 </main>
 
-<div class="text">
-  <span style="opacity: {1 - Math.max(0, y / 40)}">scroll down</span>
-
-  <div class="foreground" />
-</div>
+<div class="foreground" />
